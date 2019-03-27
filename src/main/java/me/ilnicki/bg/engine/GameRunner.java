b545376715adb6aa321c1d4930cde60c3956cbb6
Dmytro Ilnicki <dmytro@ilnicki.me>
2019-03-27 18:44:18 +0200

    Updated runner to reduce static and non-static code mixing.

--- src/main/java/me/ilnicki/bg/engine/GameRunner.java
+++ src/main/java/me/ilnicki/bg/engine/GameRunner.java
@@ -9,24 +9,27 @@ import me.ilnicki.bg.engine.io.dummy.Dummy;
 import me.ilnicki.bg.engine.machine.Machine;
 import me.ilnicki.bg.engine.pixelmatrix.loaders.DummyPixelMatrixLoader;
 import me.ilnicki.bg.engine.pixelmatrix.loaders.PixelMatrixLoader;
+import me.ilnicki.bg.engine.pixelmatrix.loaders.PixelMatrixLoaderFactory;
 import me.ilnicki.bg.engine.system.*;
 import me.ilnicki.bg.engine.system.processors.GameManager;
 import me.ilnicki.bg.engine.system.processors.SysKeysProcessor;
 
 public class GameRunner implements Runner {
-    private static boolean shouldContinue = false;
+    private boolean restartScheduled = false;
 
     private final ModuleSet modules = new ModuleSet();
     private final TickProvider tp = new TickProvider(60);
 
-    public GameRunner() {
+    public void run() {
+        modules.clear();
+
         SystemManager systemManager = new SystemManager(this);
 
         systemManager.singleton(DataProvider.class, JsonDataProvider.class);
 
         systemManager.singleton(Machine.class);
 
-        systemManager.bind(PixelMatrixLoader.class, DummyPixelMatrixLoader.class);
+        systemManager.bind(PixelMatrixLoader.class, new PixelMatrixLoaderFactory());
 
         systemManager.singleton(SysKeysProcessor.class);
         systemManager.singleton(GameManager.class);
@@ -40,9 +43,7 @@ public class GameRunner implements Runner {
         modules.addAll(systemManager.getCompatible(MachineProcessor.class));
 
         modules.load();
-    }
 
-    public void run() {
         tp.start(modules::update);
     }
 
@@ -53,14 +54,19 @@ public class GameRunner implements Runner {
 
     public void reset() {
         stop();
-        shouldContinue = true;
+        restartScheduled = true;
+    }
+
+    public boolean isRestartScheduled() {
+        return restartScheduled;
     }
 
     public static void main(String[] args) {
+        GameRunner runner = new GameRunner();
+
         do {
-            Runner runner = new GameRunner();
             runner.run();
         }
-        while (shouldContinue);
+        while (runner.isRestartScheduled());
     }
 }
