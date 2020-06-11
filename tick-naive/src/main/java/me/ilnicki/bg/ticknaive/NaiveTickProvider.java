8a6ad079d062e54312741c8bcd8083327c059fc1
ilnicki <dmytro@ilnicki.me>
2020-06-11 16:10:22 +0300

    Naive tick provider base.

---
+++ tick-naive/src/main/java/me/ilnicki/bg/ticknaive/NaiveTickProvider.java
@@ -0,0 +1,37 @@
+package me.ilnicki.bg.ticknaive;
+
+import me.ilnicki.bg.core.TickProvider;
+
+public class NaiveTickProvider implements TickProvider {
+    private final long frameTime;
+
+    private boolean isRunning = true;
+
+    public NaiveTickProvider(int tps) {
+        frameTime = Math.round(1e9 / tps);
+    }
+
+    @Override
+    public void start(TickConsumer tickConsumer) {
+        long lastTickTime = System.nanoTime();
+
+        while (isRunning) {
+            long tickStart = System.nanoTime();
+
+            tickConsumer.consume((int) (System.nanoTime() - lastTickTime));
+            lastTickTime = System.nanoTime();
+
+            while (System.nanoTime() - tickStart < frameTime) {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+
+    @Override
+    public void stop() {
+        isRunning = false;
+    }
+}
